3848e02a52790aaa897a65870c279f7baf6044b4
Darrell Cornelius Rivaldo <darrellcornelius37@gmail.com>
2023-09-23 06:58:02 +0700

    buat list produk

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AddKambingController;
+use App\Http\Controllers\AssetController;
 use App\Http\Controllers\ProdukController;
 
 /*
@@ -46,6 +47,7 @@ Route::get('/member/kontrak',[MemberController::class,'kontrak'])->name('member.
 Route::get('/add-kambing',[AddKambingController::class,'addKambingView'])->name('add-kambing-view');
 Route::post('/add-kambing/proses',[AddKambingController::class,'store'])->name('add-kambing-proses');
 
+// Halaman Penerimaan Member
 Route::get('/admin/pendaftaran',[AdminPendaftaranController::class,'index'])->name('admin.pendaftaran');
 Route::post('/admin/pendaftaran/update-status',[AdminPendaftaranController::class,'updateStatus'])->name('admin.pendaftaran.update-status');
 
@@ -56,6 +58,8 @@ Route::post('/inspeksi-kambing/proses',[AddKambingController::class,'inspeksiKam
 // Add Produk
 Route::get('/add-produk',[ProdukController::class,'addProdukView'])->name('add-produk-view');
 Route::post('/add-produk/proses',[ProdukController::class,'addProdukProses'])->name('add-produk-proses');
+Route::get('/list-produk', [ProdukController::class, 'listProduk'])->name('list-produk');
+Route::delete('/delete-produk/{id}', [ProdukController::class, 'deleteProduk'])->name('delete-produk');
 
 // Assets
 Route::get('/assets/ktp/{path}',[AssetController::class,'ktp'])->name('assets.ktp');
