5640290f385051cfd9999a209d3af90be0382458
Nico <nicos.t.65@gmail.com>
2023-09-22 13:10:50 +0700

    fix: change body margin

--- resources/js/Layout/AdminLayout.jsx
+++ resources/js/Layout/AdminLayout.jsx
@@ -79,8 +79,8 @@ const AdminLayout = ({ children }) => {
                 <style>
                     {`
                     html, body {
-                        margin: 0;
-                        padding: 0;
+                        margin: 0 !important;
+                        padding: 0 !important;
                     }
                     .ant-layout-has-sider {
                         min-height: 100vh;
