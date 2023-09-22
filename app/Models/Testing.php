903318f1fd8989e7e3a7e8c5ad033cda46d1c744
Nico <nicos.t.65@gmail.com>
2023-09-22 17:42:40 +0700

    tambah model

---
+++ app/Models/Testing.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ModelUtils;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Testing extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'content']; 
+
+    /**
+     * Rules that applied in this model
+     *
+     * @var array
+     */
+    public static function validationRules()
+    {
+        return [
+            'name' => 'required',
+            'content' => 'required',
+        ];
+    }
+
+    /**
+     * Messages that applied in this model
+     *
+     * @var array
+     */
+    public static function validationMessages()
+    {
+        return [
+            'name.required' => "Nama harus diisi",
+            'content.required' => "Content harus diisi"
+        ];
+    }
+
+    /**
+     * Filter data that will be saved in this model
+     *
+     * @var array
+     */
+    public function resourceData($request)
+    {
+        return ModelUtils::filterNullValues([]);
+    }
+
+
+    /**
+     * Controller associated with this model
+     *
+     * @var string
+     */
+
+    public function controller()
+    {
+        return 'App\Http\Controllers\TestingController';
+    }
+
+    /**
+    * Relations associated with this model
+    *
+    * @var array
+    */
+    public function relations()
+    {
+        return [];
+    }
+
+    /**
+    * Space for calling the relations
+    *
+    *
+    */
+}
