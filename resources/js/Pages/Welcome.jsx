01742e4041287761b34b9a4dc284b8032bfec31f
Nico <nicos.t.65@gmail.com>
2023-09-22 13:10:16 +0700

    fix: change body margin

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,11 +1,20 @@
+import { Head } from "@inertiajs/react";
 import AdminLayout from "../Layout/AdminLayout";
 
 import React from 'react'
 
 export default function Welcome() {
   return (
-    <AdminLayout>
-      Tes
-    </AdminLayout>
+    <>
+      <Head>
+        <meta name="name" content="Hack A Salmon" />
+        <meta name="keywords" content="Hack, Salmon, Hackathon" />
+        <meta name="description" content="Hack A Salmon!" />
+        <meta name="image" content="https://hackasalmon.online/favicon.ico" />
+      </Head>
+      <AdminLayout>
+        Tes
+      </AdminLayout>
+    </>
   )
 }
