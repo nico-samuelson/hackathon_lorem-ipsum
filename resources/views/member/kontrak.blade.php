2a4035cb04dbab7f0ede06576fe635f99fa4981e
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-09-24 00:46:35 +0700

    change route

--- resources/views/member/kontrak.blade.php
+++ resources/views/member/kontrak.blade.php
@@ -69,7 +69,13 @@
                                     </td>
                                 @endif
                                 <td>
-                                    <a href={{urlencode($k->file_kontrak_signed ?? $k->file_kontrak) }}>
+                                    <a href=
+                                        @if($k->file_kontrak_signed)
+                                            "{{ route('assets.kontrak_signed', $k->file_kontrak_signed) }}"
+                                        @else
+                                            "{{ route('assets.kontrak', $k->file_kontrak) }}"
+                                        @endif
+                                    >
                                         <button
                                         type="button"
                                         class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#EB8600] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.3),0_4px_18px_0_rgba(235,134,0,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.3),0_4px_18px_0_rgba(235,134,0,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.3),0_4px_18px_0_rgba(235,134,0,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(235,134,0,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.2),0_4px_18px_0_rgba(235,134,0,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.2),0_4px_18px_0_rgba(235,134,0,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(235,134,0,0.2),0_4px_18px_0_rgba(235,134,0,0.1)]"
